e1ca694bcfc333d3e8d6e8da53549c210e48ef0c
Arden Rasmussen <ardenisthebest@gmail.com>
2019-12-18 23:37:17 -0800

    Created build framework

---
+++ include/forma/core.hpp
@@ -0,0 +1,9 @@
+#ifndef CORE_HPP_
+#define CORE_HPP_
+
+namespace forma
+{
+  void print();
+} // namespace forma
+
+#endif // CORE_HPP_
---
+++ src/core.cpp
@@ -0,0 +1,10 @@
+#include "forma/core.hpp"
+
+#include <cstdio>
+
+namespace forma
+{
+  void print(){
+    printf("Hello World!\n");
+  }
+} // namespace forma
